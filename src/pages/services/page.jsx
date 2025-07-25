c32cad40666da1cefb324ee2e9d270b76544cf55
Maha Abo Assi <eng.mahaab96@gmail.com>
2025-07-25 18:11:43 +0400

    fix-4

--- src/pages/services/page.jsx
+++ src/pages/services/page.jsx
@@ -27,10 +27,12 @@ const Service = ()=>{
         window.scrollTo({top:0})
         const similar = services.find(e=>e.id == localStorage.getItem("similar_services"))
         let temp = []
-        similar.children.forEach((ele)=>{
+        if(similar.length > 0 ){
+            similar.children.forEach((ele)=>{
             temp = ele.services.filter(e=> e.link != `/${link}`)
             
         })
+        }
         takeFourServices(temp)
     },[link])
 
